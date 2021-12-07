532ec2ae2bdaf0b3ada51b91a7ef0f4098e887b0
HarryWang <50002801+hapsdoifh@users.noreply.github.com>
2021-12-07 17:27:01 -0500

    outputs to excel sheet properly Still bugged

--- Softball game planner.py
+++ Softball game planner.py
@@ -1,15 +1,28 @@
+#This function given an input for the teams and an input for the locations will output a list that has the optimal game order with the most unique locations.
+#It sorts it in a priority as:
+'''
+!!! Courts occupied
+!! Unique team
+! Unique location
+'''
+
 from tkinter import *
 from tkinter import ttk
 from typing import DefaultDict
 import numpy
 import pandas as pd
 from pandas.io import excel
+import numpy
+
 
 
 def RETURNMATCHES(INPUT1,INPUT2,T1,T2):
+    #Sets the list of the teams as the first input
     LISTTEAMS = INPUT1
+    #Sets the list of the locations as the second input
     LOCATIONLIST = INPUT2
-    def RETURNPOSSIBLEMATCHES(LISTTEAMS,LOCATIONCOUNTPERTEAM):
+    #Given the list of teams, this function returns every possible game as a 2d list.
+    def RETURNPOSSIBLEMATCHES(LISTTEAMS):
         TEAMNUMBER =[]
         for i in range(len(LISTTEAMS)):
             TEAMNUMBER.append(i+1)
@@ -19,36 +32,49 @@ def RETURNMATCHES(INPUT1,INPUT2,T1,T2):
                 if not(ind<=index):
                     returnlist.append([LISTTEAMS[index],team])
         return returnlist
+    #this function returns a given team to an index on the list of teams
     def TEAMTONUMBER (team):
         for index,thing in enumerate(LISTTEAMS):
             if team == thing:
                 return index
+    #Gives each team a location count of [0,0,0,0] with each 0 being a separate location.
     LOCATIONCOUNTPERTEAM = []
     for i in range(len(LISTTEAMS)):
         LOCATIONCOUNTPERTEAM.append([0,0,0,0])
-    POSSIBLEGAMES = RETURNPOSSIBLEMATCHES(LISTTEAMS,LOCATIONCOUNTPERTEAM)
+    #sets POSSIBLEGAMES to the 2d list with all possible games
+    POSSIBLEGAMES = RETURNPOSSIBLEMATCHES(LISTTEAMS)
+    #currently playing
     PLAYING = []
+    #Final Games
     FGAMES = []
+    #For each match in possible games, as long as the teams arent playing, it will append that match to final games and set each team to be playing.
+    
     while len(POSSIBLEGAMES)>0:
         for game in POSSIBLEGAMES:
             if game[0] in PLAYING or game[1] in PLAYING:
                 continue
             elif len(PLAYING)!=8:
+
+                
                 PLAYING.append(game[0])
                 PLAYING.append(game[1])
                 FGAMES.append([game[0],game[1]])
+                
             else:
                 continue
         PLAYING = []
         POSSIBLEGAMES.reverse()
-        
+    
+        # for each game in the sorted list, remove it from possible games.
         for finalized_game in FGAMES:
             if finalized_game in POSSIBLEGAMES:
                 POSSIBLEGAMES.remove(finalized_game)
     TIMEARRAY = []
     FFGAMES = []
-
+    
+    # In the end, FGAMES will be a sorted list with the order of the games so there cannot be multiple teams playing at once
     try:
+
         for i in range(0,len(FGAMES),4):
             if len(LISTTEAMS)>=8:
                 for k in range(4):
@@ -85,11 +111,11 @@ def RETURNMATCHES(INPUT1,INPUT2,T1,T2):
             LOCATIONCOUNTPERTEAM[TEAMTONUMBER(game[1])][smallestindex]+=1
     for indd,time in enumerate(FFGAMES):
         FFGAMES[indd].append(f"{indd+1}")
-    print(FFGAMES)
-
+    
     indl = 1
     TTLIST = []
     DAYSLIST = []
+    g=0
     for day in range(len(FFGAMES)):
         if (day+1)%2==0:
             DAYSLIST.append((f"Day {indl}"))
@@ -97,6 +123,7 @@ def RETURNMATCHES(INPUT1,INPUT2,T1,T2):
     DAYSLIST.append((f"Day {indl}"))
     ExcelList = []
     RowList = []
+
     for q in range(len(DAYSLIST)):
         TTLIST.append(T1)
         TTLIST.append(T2)
@@ -106,13 +133,18 @@ def RETURNMATCHES(INPUT1,INPUT2,T1,T2):
             ExcelList.append(['','','','',''])
             ExcelList.append([DAYSLIST[int(inddd/2)],'','','',''])
         print(TTLIST[inddd])
+        ExcelList.append([TTLIST[inddd],'','','',''])
         for indor,match in enumerate(timegroup):
             if isinstance(match, list):
-                ExcelList.append([FFGAMES[inddd][indor][0],'vs',FFGAMES[inddd][indor][1],'at',FFGAMES[inddd][indor][2]])
                 print(FFGAMES[inddd][indor])
+                ExcelList.append([FFGAMES[inddd][indor][0],'vs',FFGAMES[inddd][indor][1],'at',FFGAMES[inddd][indor][2]])
+                g+=1
+    if len(RETURNPOSSIBLEMATCHES(LISTTEAMS)) != g:
+        print("ERROR!! FOR SOME REASON THERE ISNT THE MAX AMOUNT OF GAMES")
+        exit()
     df = pd.DataFrame(data=ExcelList)
     df.to_excel("OUTPUT.xlsx",sheet_name= "test")
-    print()
+
 
 #INPUTS
 TeamList = [
@@ -124,7 +156,9 @@ TeamList = [
     "Team Six",
     "Team Seven",
     "Team Eight",
-    
+    "Team Nine",
+    "Team Ten",
+    "Team Eleven",
 ]
 LocationList = [
     "Location One",
@@ -142,12 +176,16 @@ tList=[]
 
 for x in range(2):
     for y in range(len(other)):
-        if(other[y][x] != numpy.nan):
+        if not pd.isnull(other[y][x]):
             if x == 0:
                 tList.append(str(other[y][x]))
             else:
                 lList.append(str(other[y][x]))
         else:
             break
-RETURNMATCHES(tList,lList,TimeOne,TimeTwo)
+RETURNMATCHES(TeamList,LocationList,TimeOne,TimeTwo)
 
+print(tList)
+print(lList)
+print(TeamList)
+print(LocationList)
