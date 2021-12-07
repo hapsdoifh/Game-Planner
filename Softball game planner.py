74161af8cab8633018f34b77c31a16713eb0c604
HarryWang <zuhanwang@outlook.com>
2021-12-07 18:04:28 -0500

    Creates a file dialog

--- Softball game planner.py
+++ Softball game planner.py
@@ -1,37 +1,36 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog
 import pandas as pd
 
-df = pd.read_excel('Book1.xlsx')
-df.head()
-print(df.shape)
 
 root = Tk()
 root.geometry("300x300")
-myCanv = Canvas(root,height = 1080, width = 1920)
-myCanv.pack()
-global myl
-myl = Label(root,text="temp")
-myl.place(x=10,y=60)   
+
+myCanv = Canvas(root,height = 1000, width = 1000)
+myCanv.pack()   
 mylb = Label(root,text="temp")
 mylb.place(x=10,y=40) 
 myCircle = myCanv.create_oval(0,0,50,50,fill="red",width=5,outline="red") 
 
-myCanv.move(myCircle,30,30)
-myCanv.move(myCircle,30,30)
-myCanv.move(myCircle,30,30)
+global myl
+myl = Label(root,text="myfile")
+
 def CanvResize(event):
     xval = event.width
     yval = event.height
-   #myl.configure(text = str(xval))
-    myl.configure(text = df)
     mylb.configure(text = str(yval))
+    myl.place(x=xval/2-100,y=yval/2-100)
     myCanv.moveto(myCircle,xval/2-30,yval/2-30)
     
-def DestroyCanv():
-    myCanv.delete("all")
+def GetFile():
+    filename = filedialog.askopenfilename(filetypes=(("xlsx file","*.xlsx"),("xlsx file","*.xlsx")))
+    myfile = pd.read_excel(filename)
+    myl.configure(text = myfile)
+    print(myfile)
+
     
-desbutton = Button(root,text="destroy",command=DestroyCanv)
+desbutton = Button(root,text="Get File",command=GetFile)
 myCanv.bind("<Configure>",CanvResize)
 desbutton.place(x=10,y=10)
 
